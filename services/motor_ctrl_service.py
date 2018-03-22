821ab440dc1b6009d325bfea48423b7a96453984
Amir Sasson <amirsas@gmail.com>
2018-03-22 14:58:52 +0200

    json parse..

--- services/motor_ctrl_service.py
+++ services/motor_ctrl_service.py
@@ -62,5 +62,5 @@ class MotorCtrlService(object):
         logging.debug("acting !!! -> " + str(msg) + " left:" +
                       str(msg["speed_left"]))
         self.move(
-            speed_left=float(msg.speed_left),
-            speed_right=float(msg.speed_right))
+            speed_left=float(msg["speed_left"]),
+            speed_right=float(msg["speed_right"]))
