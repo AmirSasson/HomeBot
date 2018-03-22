3abe5c76ee939ebbb45af5fe13d0ad12e8b59ab3
Amir Sasson <amirsas@gmail.com>
2018-03-22 14:40:24 +0200

    add motor control

--- services/motor_ctrl_service.py
+++ services/motor_ctrl_service.py
@@ -1,6 +1,9 @@
 """FB Service Module"""
 import logging
 import settings as Config
+from gpiozero import Motor
+import time
+import math
 
 
 # pylint: disable=too-few-public-methods
@@ -10,7 +13,51 @@ class MotorCtrlService(object):
     def __init__(self):
         # self.graph = facebook.GraphAPI(Config.PAGE_ACCESS_TOKEN)
         logging.debug("initializing motor control!")
+        self.motor_right = Motor(17, 18)
+        self.motor_left = Motor(6, 12)
+        self.start()
+
+    def start(self):
+        self.full_spin()
+
+    def full_spin(self):
+        self.stop()
+        self.motor_left.forward(1)
+        self.motor_left.backward(1)
+        time.sleep(0.5)
+        self.stop()
+
+    def forward(self, speed=1):
+        self.move()
+
+    def backward(self, speed=1):
+        self.move(-1, -1)
+
+    def move(self, speed_right=1, speed_left=1):
+        self.stop()
+        if speed_right > 0:
+            self.motor_right.forward(speed_right)
+            pass
+        elif speed_right < 0:
+            self.motor_right.backward(math.fabs(speed_right))
+        else:
+            self.motor_right.stop()
+
+        if speed_left > 0:
+            self.motor_left.forward(speed_left)
+        elif speed_left < 0:
+            self.motor_left.backward(math.fabs(speed_left))
+        else:
+            self.motor_left.stop()
+
+    def stop(self):
+        self.motor_right.stop()
+        self.motor_left.stop()
 
     def act(self, msg):
         """perform motor move"""
-        logging.debug("acting !!! -> " + str(msg))
+        logging.debug("acting !!! -> " + str(msg) + " left:" +
+                      str(msg.speed_left))
+        self.move(
+            speed_left=float(msg.speed_left),
+            speed_right=float(msg.speed_right))
