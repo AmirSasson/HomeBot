0eeafb788bc5c662e2f54cfac2ab17fb677eaec3
Amir Sasson <amirsas@gmail.com>
2018-04-17 22:10:33 +0300

    fix

--- services/nav_service.py
+++ services/nav_service.py
@@ -44,14 +44,14 @@ class NavService(object):
 
     def _dist_check(self):
         dist_cm = self.sensor.distance * 100
-        if math.fabs(self.last_known_distance_cm - dist_cm) > 3:
+        if math.fabs(self.last_known_distance_cm - dist_cm[0]) > 3:
             logging.info('Distance: %(dist_cm)s cm')
-        if (not self.last_known_distance_cm == dist_cm
+        if (not self.last_known_distance_cm == dist_cm[0]
             ) and int(dist_cm[0]) <= MIN_DIST_CM:
             logging.info('Distance Sensor Stopping motor!!')
             stop_msg = {'speed_left': 0, 'speed_right': 0}
             self.event_emitter.emit(self.topic_motor, stop_msg)
-        self.last_known_distance_cm = dist_cm
+        self.last_known_distance_cm = dist_cm[0]
 
 
 if __name__ == '__main__':
