a732ad4ac20f7c22477d226c944b574faad09bab
Amir Sasson <amirsas@gmail.com>
2018-04-17 19:16:47 +0300

    set thresholds

--- services/nav_service.py
+++ services/nav_service.py
@@ -2,7 +2,6 @@
 import logging
 from gpiozero import DistanceSensor
 from pyee import EventEmitter
-from time import sleep
 
 MIN_DIST_CM = 5
 
@@ -18,14 +17,18 @@ class NavService(object):
         # https://www.bluetin.io/sensors/python-library-ultrasonic-hc-sr04/
         self.topic_motor = motor_topic
 
-        self.sensor = DistanceSensor(echo=24, trigger=23, queue_len=2)
+        self.sensor = DistanceSensor(
+            echo=24,
+            trigger=23,
+            queue_len=2,
+            threshold=0.01, )
         self.sensor.when_activated = self._dist_check
         self.sensor.when_out_of_range = self._dist_check
 
     def _dist_check(self):
         dist_cm = self.sensor.distance * 100,
         logging.info('Distance: %(dist_cm)f cm')
-        print('Distance: ' + str(dist_cm[0]) + ' cm')
+        print('Distance: ' + str(dist_cm) + ' cm')
         if int(dist_cm[0]) <= MIN_DIST_CM:
             logging.info('Distance Sensor Stopping motor!!')
             stop_msg = {'speed_left': 0, 'speed_right': 0}
